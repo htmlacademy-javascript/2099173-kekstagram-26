1e8490761103cbfc3ac4ca86cc13fce6699ad970
Andrei Gladyshev <mytime12-61pm@yandex.ru>
2022-06-06 14:23:57 +0400

    1) Возвращает случайное целое число из переданного диапазона включительно; 2) Проверяет максимальную длину строки

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
-function summirize(firstNumber, secondNumber) {
-  return firstNumber + secondNumber;
-}
+// Функция, возвращающая случайное целое число из переданного диапазона включительно.
 
-summirize();
+function getNumberFrom (minValue, maxValue) {
+  const providedNumbers = [];
+  let increaser = 0;
+  if (minValue >= maxValue) {
+    return 'Ошибка: первое число должно быть меньше второго!';
+  } else if (minValue % 1 !==0 || maxValue % 1 !==0) {
+    return 'Ошибка: оба числа должны быть целыми!';
+  } else if (minValue <0 || maxValue < 0) {
+    return 'Ошибка: оба числа должны быть положительными!';
+  } else {
+    for (let i = 0; i <= maxValue - minValue; i++) {
+      providedNumbers[i] = minValue + increaser;
+      increaser++;
+    }
+  }
+  return providedNumbers[Math.floor(Math.random() * providedNumbers.length)];
+}
+getNumberFrom();
+
+
+// Функция для проверки максимальной длины строки.
+
+function limitString (string, maxLength) {
+  return (string.length <= maxLength);
+}
+limitString();
